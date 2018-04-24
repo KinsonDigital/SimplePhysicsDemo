b7ffc3d8514b7f5ba2ede86281889947fe7b7c67
KinsonDigital <kinsondigital@gmail.com>
2018-04-24 12:16:39 -0400

    Create World class

---
+++ GravityTesting/World.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravityTesting
+{
+    /// <summary>
+    /// Represents a game world of <see cref="GameObject"/>s with physics with the world having gravity, air/fluid density, and size.
+    /// </summary>
+    public class World
+    {
+    }
+}
